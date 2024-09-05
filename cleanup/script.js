f80261d1643b98aa9d5db7a82b59c2e29b88c32d
Mostafa Miandari Hossein <mk@Mostafas-MacBook-Pro.local>
2024-09-05 19:49:05 -0400

    made it wss

--- cleanup/script.js
+++ cleanup/script.js
@@ -1,5 +1,5 @@
 // Replace with your EC2 WebSocket server public IP
-const socket = new WebSocket("ws://18.117.180.219:8765");
+const socket = new WebSocket("wss://18.117.180.219:8765");
 
 socket.onopen = function() {
     console.log("Connected to WebSocket server.");
